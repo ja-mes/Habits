28ca2f0f238bd62306c578850f58364cac25b727
James <jsbrown000@gmail.com>
2016-09-18 15:34:20 -0500

    Setup correct number of sections

--- Habits/HabitsViewController.swift
+++ Habits/HabitsViewController.swift
@@ -24,14 +24,30 @@ class HabitsViewController: UIViewController, UITableViewDelegate, UITableViewDa
     
     // MARK: table protocols
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        if let sections = controller.sections {
+            return sections.count
+        }
+        
+        return 0g
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        if let sections = controller.sections {
+            return sections[section].numberOfObjects
+        }
+        
+        return 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "HabitCell") {
+            
+            cell.textLabel?.text = "foo"
+            
+            return cell
+            
+        }
+        
         return UITableViewCell()
     }
 
