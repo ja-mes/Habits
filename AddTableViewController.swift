10b3dca06236836d95b48e5c52bf38aed475d9f7
James <jsbrown000@gmail.com>
2016-09-18 14:47:33 -0500

    Create tableview class

---
+++ AddTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AddTableViewController.swift
+//  Habits
+//
+//  Created by James Brown on 9/18/16.
+//  Copyright © 2016 James Brown. All rights reserved.
+//
+
+import UIKit
+
+class AddTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
