7de8f2068354d1e7a62553e9916846f1c7e0ebc1
James <jsbrown000@gmail.com>
2016-09-27 19:21:25 -0500

    add color cell and remove streak cell

---
+++ Habits/ColorTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ColorTableViewCell.swift
+//  Habits
+//
+//  Created by James Brown on 9/27/16.
+//  Copyright © 2016 James Brown. All rights reserved.
+//
+
+import UIKit
+
+class ColorTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
