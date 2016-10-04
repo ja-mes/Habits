4cc11d4220cacf3442f3b1dbbd487a8cc93136d3
James <jsbrown000@gmail.com>
2016-10-04 16:21:40 -0500

    Recreate classes

---
+++ Habits/Habit+CoreDataProperties.swift
@@ -0,0 +1,23 @@
+//
+//  Habit+CoreDataProperties.swift
+//  Habits
+//
+//  Created by James Brown on 10/4/16.
+//  Copyright © 2016 James Brown. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+extension Habit {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Habit> {
+        return NSFetchRequest<Habit>(entityName: "Habit");
+    }
+
+    @NSManaged public var days: Int64
+    @NSManaged public var lastEntry: NSDate?
+    @NSManaged public var name: String?
+
+}
